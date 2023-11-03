11a6842e4bb668d065145527e899a4c7c1cacedc
Tris <gingertris1@gmail.com>
2023-11-03 13:15:03 +0000

    i got something working ish

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Map from "$lib/Map.svelte";
+	import type { Marker } from "leaflet";
+    import type {Map as LMap} from 'leaflet'
+    let selectedMarker: Marker | null;
+    let existingMarkers: Marker[]
+    let map: LMap;
+
+    function addMarker() {
+        if(!selectedMarker) return
+        existingMarkers = [...existingMarkers, selectedMarker];
+        selectedMarker.removeFrom(map);
+        selectedMarker = null
+    }
+
+</script>
+
+
+
+<h1>GeoNotes</h1>
+<Map bind:selectedMarker bind:existingMarkers bind:map />
+
+<div>
+    <h3>Selected</h3>
+    <p>Lat: {selectedMarker ? selectedMarker.getLatLng().lat : 'null'}</p>
+    <p>Lng: {selectedMarker ? selectedMarker.getLatLng().lng : 'null'}</p>
+
+    <button on:click={addMarker}>Add</button>
+</div>
+
+
+
