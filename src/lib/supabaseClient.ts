7dedc41144cb011f85d4a325365b69b52b1145af
Tristan Derrill <gingertris1@gmail.com>
2023-11-03 10:41:17 +0000

    clearing

--- src/lib/supabaseClient.ts
+++
@@ -1,3 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { SUPABASE_URL, SUPABASE_TOKEN } from "$env/static/private";
-export const supabase = createClient(SUPABASE_URL, SUPABASE_TOKEN)
