c2e5dade977d263b6a43de351812f23ebd756d99
Tristan Derrill <gingertris1@gmail.com>
2023-11-03 01:39:17 +0000

    supabase initial commit

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,3 @@
+import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL, SUPABASE_TOKEN } from "$env/static/private";
+export const supabase = createClient(SUPABASE_URL, SUPABASE_TOKEN)
