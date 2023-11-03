c2ccb0b70b60eb0bf05b2b6a7e16be6077a7d81e
Tris <gingertris1@gmail.com>
2023-11-03 14:25:01 +0000

    goodbye supabase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
-	import Map from "$lib/Map.svelte";
-	import type { Marker } from "leaflet";
-    import type {Map as LMap} from 'leaflet'
-    let selectedMarker: Marker | null;
-    let existingMarkers: Marker[]
-    let map: LMap;
-
-    function addMarker() {
-        if(!selectedMarker) return
-        existingMarkers = [...existingMarkers, selectedMarker];
-        selectedMarker.removeFrom(map);
-        selectedMarker = null
-    }
-
+    import Map from "$lib/Map.svelte";
+    import type {Marker, Map as LMap} from 'leaflet';
+	let selectedMarker:Marker | null, existingMarkers:Marker[], map: LMap;
 </script>
 
 
@@ -21,11 +10,7 @@
 <Map bind:selectedMarker bind:existingMarkers bind:map />
 
 <div>
-    <h3>Selected</h3>
-    <p>Lat: {selectedMarker ? selectedMarker.getLatLng().lat : 'null'}</p>
-    <p>Lng: {selectedMarker ? selectedMarker.getLatLng().lng : 'null'}</p>
 
-    <button on:click={addMarker}>Add</button>
 </div>
 
 
