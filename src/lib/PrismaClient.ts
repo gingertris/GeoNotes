89b3ef1a6b75034170dcf9411fafd3952e942acb
Tris <gingertris1@gmail.com>
2023-11-06 21:59:53 +0000

    prisma...

--- src/lib/PrismaClient.ts
+++ src/lib/PrismaClient.ts
@@ -1,7 +1,6 @@
-import { DATABASE_URL } from '$env/static/private';
 import {PrismaClient} from '@prisma/client/edge'
 
 
 export const prisma = new PrismaClient({
-  datasources: { db: { url: DATABASE_URL } }
+  datasources: { db: { url: process.env.DATABASE_URL } }
 });
