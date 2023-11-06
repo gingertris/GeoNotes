d3ec29c1b26280fcf87c5440b1f16f3693c5291a
Tris <gingertris1@gmail.com>
2023-11-06 21:58:29 +0000

    prisma fix?

--- src/lib/PrismaClient.ts
+++ src/lib/PrismaClient.ts
@@ -1,3 +1,7 @@
+import { DATABASE_URL } from '$env/static/private';
 import {PrismaClient} from '@prisma/client/edge'
 
-export const prisma = new PrismaClient();
+
+export const prisma = new PrismaClient({
+  datasources: { db: { url: DATABASE_URL } }
+});
