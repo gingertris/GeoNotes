74cd77ec5df9d226b5ba50f7509fe00087ebff89
Tris <gingertris1@gmail.com>
2023-11-06 22:10:09 +0000

    external marker logo

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -9,7 +9,28 @@
     export let selectedNote: Note|null = null;
 
     async function createMap(container:HTMLElement){
-        const L = await import("leaflet")
+        const L = await import("leaflet");
+
+        const icons = {
+            red: new L.Icon({
+                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+                shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowSize: [41, 41]
+            }),
+            blue: new L.Icon({
+                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+                shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowSize: [41, 41]
+            })
+        }
+
+
         let m = L.map(container).setView([51.505, -0.09], 5)
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -19,23 +40,18 @@
         m.on('click', e => {
             if(selectedMarker) selectedMarker.removeFrom(m)
             console.log(e)
-            selectedMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(m)
+            selectedMarker = L.marker([e.latlng.lat, e.latlng.lng], {
+                icon:icons.blue
+            }).addTo(m)
             selectedNote = null;
         })
 
-        var redIcon = new L.Icon({
-            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-            iconSize: [25, 41],
-            iconAnchor: [12, 41],
-            popupAnchor: [1, -34],
-            shadowSize: [41, 41]
-        });
+
 
         for(let note of notes){
             let marker = L.marker([note.lat, note.lng], {
                 title: note.title,
-                icon:redIcon
+                icon:icons.red
             }).bindPopup(note.title);
             marker.on('click', ()=>{
                 selectedNote = note;
